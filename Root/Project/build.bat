a98f7f808ced826652b7bd73b3370fc1594715dd
Dmitry Kulik <dmitry.kulik@forcepoing.com>
2019-04-04 13:18:41 +0300

    relative path in bat file + return error code from bat file

--- Root/Project/build.bat
+++ Root/Project/build.bat
@@ -3,18 +3,23 @@ echo off
 set devenv="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"
 del build_output.log
 
-%devenv% "Project.sln" /Rebuild "Release|x64" /out build_output.log
+%devenv% "%~dp0\Project.sln" /Rebuild "Release|x64" /out build_output.log
 IF NOT %ERRORLEVEL% == 0 (
 echo "Project Release|x64 Error"
 goto error )
 echo "Project Release|x64 OK"
 
-%devenv% "Project.sln" /Rebuild "Release|x86" /out build_output.log
+%devenv% "%~dp0\Project.sln" /Rebuild "Release|x86" /out build_output.log
 IF NOT %ERRORLEVEL% == 0 (
 echo "Project Release|x86 Error"
 goto error )
 echo "Project Release|x86 OK"
 
-:error
 
-echo Finish
+echo Successfully finished building
+
+exit /b 0
+
+:error
+exit /b -1
+
